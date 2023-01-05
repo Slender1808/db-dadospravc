e24f7079b63bb529ff101bea0b621fa76b8dcba3
jadsongmatos <jadson.g-matos@outlook.com>
2023-01-05 19:46:17 -0300

    basico ok

--- basic.js
+++ basic.js
@@ -1,20 +1,68 @@
-/*import startBrowser from "./browser.mjs";
+/*
+import startBrowser from "./browser.mjs";
 
 let browser = await startBrowser();
 const page = await browser.newPage();
 
-await page.goto("http://0.0.0.0:8080/", { waitUntil: "domcontentloaded" });*/
+await page.goto("http://0.0.0.0:8080/pai/", { waitUntil: "domcontentloaded" });
+await page.close();
+await browser.close();
+*/
 
+/*
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("/tmp/db.sqlite");
 
 db.parallelize(() => {
   db.all(
     `
-      SELECT * 
+      SELECT path 
       FROM file 
-      WHERE data MATCH 'Acre' 
+      WHERE data MATCH 'estados' 
       ORDER BY rank;`,
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+      }
+    }
+  );
+});
+*/
+
+/*
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database("/tmp/db.sqlite");
+
+db.parallelize(() => {
+  db.all(
+    `
+      SELECT path 
+      FROM file 
+      WHERE data MATCH 'estados' 
+      ORDER BY rank;`,
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+      }
+    }
+  );
+});
+*/
+
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database("/tmp/db.sqlite");
+
+db.parallelize(() => {
+  db.get(
+    `
+      SELECT path 
+      FROM file 
+      WHERE path = (?);`,
+    "http://0.0.0.0:8080/pai/",
     (err, data) => {
       if (err) {
         console.log(err);
--- main.js
+++ main.js
@@ -27,63 +27,104 @@ async function insert(href) {
   return new Promise((resolve, reject) => {
     console.log(href);
 
-    db.run(
-      "INSERT INTO metadata (path) VALUES (?);",
-      href,
-      async function (err) {
-        if (err) {
-          if (err.errno != 19) {
-            console.log("insert_metadata --", err);
-            return reject(err);
-          }
-        }
-        console.log("insert_metadata ++", this);
-        try {
-          const response = await fetch(href);
-          console.log(response.headers.get("Content-Type"));
+    db.get("SELECT * FROM metadata WHERE path = (?);", href, (err, row) => {
+      if (err) {
+        console.log("duplicado metadata");
+        return reject(err);
+      } else {
+        console.log(row);
+        if (row == undefined) {
+          db.run(
+            "INSERT INTO metadata (path) VALUES (?);",
+            href,
+            function (err) {
+              if (err) {
+                console.log("insert_metadata --", err);
+                return reject(err);
+              }
+              console.log("insert_metadata ++", this);
 
-          if (response.headers.get("Content-Type").includes("text/html")) {
-            const page = await browser.newPage();
-            await page.goto(href, { waitUntil: "domcontentloaded" });
-
-            data = await page.content();
-            page.close();
-
-            db.run(
-              "insert into file (path,data) values (?,?)",
-              [href, data],
-              async function (err) {
-                if (err) {
-                  if (err.errno != 19) {
-                    console.log("insert_file_data --", err);
+              db.get(
+                `
+                  SELECT path 
+                  FROM file 
+                  WHERE path = (?);`,
+                "http://0.0.0.0:8080/pai/",
+                async (err, data) => {
+                  if (err) {
+                    console.log("duplicado file");
                     return reject(err);
+                  } else {
+                    console.log(data);
+                    if (data == undefined) {
+                      try {
+                        const response = await fetch(href);
+                        console.log(response.headers.get("Content-Type"));
+
+                        if (
+                          response.headers
+                            .get("Content-Type")
+                            .includes("text/html")
+                        ) {
+                          console.log("newPage", href);
+                          const page = await browser.newPage();
+                          console.log("goto");
+                          await page.goto(href, {
+                            waitUntil: "domcontentloaded",
+                          });
+
+                          data = await page.content();
+                          await page.close();
+
+                          db.run(
+                            "insert into file (path,data) values (?,?)",
+                            [href, data],
+                            function (err) {
+                              console.log("insert_file_data", this);
+                              if (err) {
+                                if (err.errno != 19) {
+                                  console.log("insert_file_data --", err);
+                                  return reject(err);
+                                }
+                              }
+                              return resolve();
+                            }
+                          );
+
+                          //await scrap(await page.$$eval("a", (as) => as.map((a) => a.href)));
+                        } else {
+                          buffer = await response.buffer();
+
+                          db.run(
+                            "insert into file (path,data) values (?,?)",
+                            [href, buffer],
+                            async function (err) {
+                              if (err.errno != 19) {
+                                console.log("insert_file_buffer --", err);
+                                return reject(err);
+                              }
+                              resolve();
+                            }
+                          );
+                        }
+                      } catch (err) {
+                        // An error occurred
+                        console.log("catch", err);
+                        reject(err);
+                      }
+                    } else {
+                      return resolve();
+                    }
                   }
                 }
-              }
-            );
-
-            //await scrap(await page.$$eval("a", (as) => as.map((a) => a.href)));
-          } else {
-            buffer = await response.buffer();
-
-            db.run(
-              "insert into file (path,data) values (?,?)",
-              [href, buffer],
-              async function (err) {
-                if (err.errno != 19) {
-                  console.log("insert_file_buffer --", err);
-                  return reject(err);
-                }
-              }
-            );
-          }
-          resolve();
-        } catch (err) {
-          // An error occurred
-          reject(err);
+              );
+            }
+          );
+        } else {
+          return resolve();
         }
       }
-    );
+    });
   });
 }
 
@@ -94,8 +135,6 @@ db.serialize(() => {
     })
     .finally(async () => {
       console.log("scrap finally --------");
-      //insert_metadata.finalize();
-      //insert_file.finalize();
       db.close();
       await browser.close();
     });
