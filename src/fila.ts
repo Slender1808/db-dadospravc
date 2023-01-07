db327296403ab7113358aef7e569a7ddb64f8070
jadsongmatos <jadson.g-matos@outlook.com>
2023-01-07 01:53:25 -0300

    async

--- src/fila.ts
+++ src/fila.ts
@@ -22,6 +22,10 @@ const q: Queue = new Queue(asyncWorker, {
   store: store,
 });
 
+q.on("task_failed", function (taskId: any, err: any, stats: any) {
+  console.log("task_finish", taskId, err, stats);
+});
+
 var browser: any;
 
 main();
@@ -34,126 +38,102 @@ async function main() {
   await scrap(hrefs);
 }
 
-async function scrap(hrefs: string[]) {
-  return Promise.all(hrefs.map((href) => {
+function scrap(hrefs: string[]) {
+  hrefs.map((href) => {
     q.push(href);
-  }))
+  });
 }
 
-async function asyncWorker(href: string,cb:any): Promise<any> {
-  return new Promise((resolve, reject) => {
-    console.log(href);
-
-    db.get("SELECT * FROM metadata WHERE path = (?);", href, (err, row) => {
-      if (err) {
-        console.log("duplicado metadata");
-        cb(null, null);
-        return reject(err);
-      } else {
-        if (row == undefined) {
-          db.run(
-            "INSERT INTO metadata (path) VALUES (?);",
-            href,
-            function (err) {
-              if (err) {
-                console.log("insert_metadata --", err);
-                cb(null, null);
-                return reject(err);
-              }
-              db.get(
-                `
+async function asyncWorker(href: string, cb: any): Promise<any> {
+  console.log(href);
+  db.get("SELECT * FROM metadata WHERE path = (?);", href, (err, row) => {
+    if (err) {
+      console.log("duplicado metadata");
+      cb(null, null);
+    } else {
+      if (row == undefined) {
+        db.run("INSERT INTO metadata (path) VALUES (?);", href, function (err) {
+          if (err) {
+            console.log("insert_metadata --", err);
+            cb(null, null);
+          }
+          db.get(
+            `
                   SELECT path 
                   FROM file 
                   WHERE path = (?);`,
-                "http://0.0.0.0:8080/pai/",
-                async (err, data) => {
-                  if (err) {
-                    console.log("duplicado file");
-                    cb(null, null);
-                    return reject(err);
-                  } else {
-                    if (data == undefined) {
-                      try {
-                        const response = await fetch(href);
-                        console.log(response.headers.get("Content-Type"));
+            "http://0.0.0.0:8080/pai/",
+            async (err, data) => {
+              if (err) {
+                console.log("duplicado file");
+                cb(null, null);
+              } else {
+                if (data == undefined) {
+                  try {
+                    const response = await fetch(href);
+                    console.log(response.headers.get("Content-Type"));
 
-                        const content_type: any =
-                          response.headers.get("Content-Type");
+                    const content_type: any =
+                      response.headers.get("Content-Type");
 
-                        if (content_type.includes("text/html")) {
-                          const page = await browser.newPage();
-                          await page.goto(href, {
-                            waitUntil: "domcontentloaded",
-                          });
+                    if (content_type.includes("text/html")) {
+                      const page = await browser.newPage();
+                      await page.goto(href, {
+                        waitUntil: "domcontentloaded",
+                      });
 
-                          data = await page.content();
-                          const new_hrefs = await page.$$eval("a", (as: any) =>
-                            as.map((a: any) => {
-                              let url = new URL(a.href);
-                              return url.origin + url.pathname;
-                            })
-                          );
-                          await page.close();
+                      data = await page.content();
+                      const new_hrefs = await page.$$eval("a", (as: any) =>
+                        as.map((a: any) => {
+                          let url = new URL(a.href);
+                          return url.origin + url.pathname;
+                        })
+                      );
+                      await page.close();
 
-                          db.run(
-                            "insert into file (path,data) values (?,?)",
-                            [href, data],
-                            async function (err: any) {
-                              console.log("insert_file_data", this);
-                              if (err) {
-                                if (err.errno != 19) {
-                                  console.log("insert_file_data --", err);
-                                  cb(null, null);
-                                  return reject(err);
-                                }
-                              }
-                              await scrap(new_hrefs)
-                                .then(() => {
-                                  cb(null, null);
-                                  return resolve(null);
-                                })
-                                .catch((err) => {
-                                  cb(null, null);
-                                  return reject(err);
-                                });
-                            }
-                          );
-                        } else {
-                          const buffer = await response.buffer();
-
-                          db.run(
-                            "insert into file (path,data) values (?,?)",
-                            [href, buffer],
-                            async function (err) {
-                              if (err) {
-                                console.log("insert_file_buffer --", err);
-                                return reject(err);
-                              }
+                      db.run(
+                        "insert into file (path,data) values (?,?)",
+                        [href, data],
+                        async function (err: any) {
+                          console.log("insert_file_data", this);
+                          if (err) {
+                            if (err.errno != 19) {
+                              console.log("insert_file_data --", err);
                               cb(null, null);
-                              return resolve(null);
                             }
-                          );
+                          }
+                          scrap(new_hrefs);
                         }
-                      } catch (err) {
-                        // An error occurred
-                        console.log("catch", err);
-                        cb(null, null);
-                        reject(err);
-                      }
+                      );
                     } else {
-                      cb(null, null);
-                      return resolve(null);
+                      const buffer = await response.buffer();
+
+                      db.run(
+                        "insert into file (path,data) values (?,?)",
+                        [href, buffer],
+                        async function (err) {
+                          if (err) {
+                            console.log("insert_file_buffer --", err);
+                          }
+                          cb(null, null);
+                        }
+                      );
                     }
+                  } catch (err) {
+                    // An error occurred
+                    console.log("catch", err);
+                    cb(null, null);
                   }
+                } else {
+                  cb(null, null);
                 }
-              );
+              }
             }
           );
-        } else {
-          cb(null, null);
-          return resolve(null);
-        }
+        });
+      } else {
+        cb(null, null);
       }
-    });
+    }
   });
 }
