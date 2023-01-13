88c2ce0e418ed11819273918c998e22e034739ab
jadsongmatos <jadson.g-matos@outlook.com>
2023-01-13 01:09:56 -0300

    estavel

--- src/fila.ts
+++ src/fila.ts
@@ -17,47 +17,50 @@ const store = new SqliteStore({
   path: "/tmp/queue.db.sqlite",
 });
 
+var batch_finish = false;
+var drain_count = 0;
+var intervalId: any;
+
 const q: Queue = new Queue(asyncWorker, {
   concurrent: 16,
   store: store,
-  maxTimeout: 3000,
   maxRetries: 3,
   retryDelay: 1000,
 });
 
 q.on("task_failed", function (taskId: any, err: any, stats: any) {
   console.log("task_finish", taskId, err, stats);
-  console.log();
+});
+
+q.on("task_failed", function (taskId: any, err: any, stats: any) {
+  console.log("task_failed", taskId, err, stats);
 });
 
 q.on("empty", function () {
   console.log("empty");
-});
-
-var batch_finish = false;
-var drain_count = 0;
-q.on("batch_finish", function () {
   batch_finish = true;
+  drain_count = 0;
+  finally_proc();
 });
 
 q.on("task_queued", function () {
   batch_finish = false;
+  clearInterval(intervalId);
 });
-
-const intervalId = setInterval(() => {
-  drain_count = drain_count + (batch_finish ? 1 : 0);
-  console.log(drain_count);
-  if (drain_count === 16) {
-    clearInterval(intervalId);
-    finally_proc();
-    drain_count = 0;
-  }
-}, 3000);
 
 async function finally_proc() {
   const numberOfOpenPages = (await browser.pages()).length;
   console.log("number Of Open Pages", numberOfOpenPages);
   if (numberOfOpenPages == 0) {
+    intervalId = setInterval(() => {
+      drain_count = drain_count + (batch_finish ? 1 : 0);
+      console.log(drain_count);
+      if (drain_count === 16) {
+        clearInterval(intervalId);
+        console.log("finally");
+        drain_count = 0;
+      }
+    }, 3000);
     console.log("finally");
   }
 }
@@ -76,16 +79,16 @@ async function main() {
   let hrefs = ["https://en.wikipedia.org/wiki/Main_Page"];
   console.log(hrefs);
 
-  await scrap(hrefs);
+  scrap(hrefs);
 }
 
-function scrap(hrefs: string[]) {
+async function scrap(hrefs: string[]) {
   hrefs.map((href) => {
     q.push(href);
   });
 }
 
-async function asyncWorker(href: string, cb: any): Promise<any> {
+async function asyncWorker(href: string, cb: any) {
   console.log(href);
   db.get("SELECT * FROM metadata WHERE path = (?);", href, (err, row) => {
     if (err) {
@@ -112,59 +115,72 @@ async function asyncWorker(href: string, cb: any): Promise<any> {
                 if (data == undefined) {
                   let page;
                   try {
-                    const response = await fetch(href);
-                    console.log(response.headers.get("Content-Type"));
+                    const response = await fetch(href, {
+                      retry: {
+                        maxTimeout: 5000,
+                      },
+                    });
+                    //console.log(response.headers.get("Content-Type"));
 
                     const content_type: any =
                       response.headers.get("Content-Type");
 
                     if (content_type.includes("text/html")) {
                       page = await browser.newPage();
-                      await page.goto(href, {
-                        waitUntil: "domcontentloaded",
-                      });
-                      data = await page.content();
-
                       try {
-                        const links_e = await page.$$("a");
-                        let new_hrefs: Array<string> = [];
-                        for (const link of links_e) {
-                          new_hrefs.push(
-                            await link.evaluate((node: any) =>
-                              node.getAttribute("href")
-                            )
-                          );
-                        }
-
-                        new_hrefs = new_hrefs.map((new_href: string) => {
-                          try {
-                            new URL(new_href);
-                            return new_href;
-                          } catch (err) {
-                            let url = new URL(href);
-                            return url.origin + new_href;
-                          }
+                        await page.goto(href, {
+                          waitUntil: "load",
+                          timeout: 5000,
                         });
+                        data = await page.content();
 
-                        await page.close();
-
-                        db.run(
-                          "insert into file (path,data) values (?,?)",
-                          [href, data],
-                          async function (err: any) {
-                            console.log("insert_file_data", this);
-                            if (err) {
-                              if (err.errno != 19) {
-                                console.log("insert_file_data --", err);
-                                cb(null, null);
-                              }
-                            }
-                            scrap(new_hrefs);
+                        try {
+                          const links_e = await page.$$("a");
+                          let new_hrefs: Array<string> = [];
+                          for (const link of links_e) {
+                            new_hrefs.push(
+                              await link.evaluate((node: any) =>
+                                node.getAttribute("href")
+                              )
+                            );
                           }
-                        );
-                      } catch (err) {
+
+                          new_hrefs = new_hrefs.map((new_href: string) => {
+                            try {
+                              new URL(new_href);
+                              return new_href;
+                            } catch (err) {
+                              let url = new URL(href);
+                              return url.origin + new_href;
+                            }
+                          });
+
+                          await page.close();
+
+                          db.run(
+                            "insert into file (path,data) values (?,?)",
+                            [href, data],
+                            async function (err: any) {
+                              console.log("insert_file_data", this);
+                              if (err) {
+                                if (err.errno != 19) {
+                                  console.log("insert_file_data --", err);
+                                  cb(null, null);
+                                }
+                              }
+
+                              scrap(new_hrefs);
+                              cb(null, null);
+                            }
+                          );
+                        } catch (err) {
+                          await page.close();
+                          console.log("catch get links", err);
+                          cb(err);
+                        }
+                      } catch (error) {
                         await page.close();
-                        console.log("catch get links", err);
+                        console.log("catch page.goto", err, href);
                         cb(err);
                       }
                     } else {
@@ -184,9 +200,10 @@ async function asyncWorker(href: string, cb: any): Promise<any> {
 
                       cb(null, null);
                     }
-                  } catch (err) {
-                    // An error occurred
-                    console.log("catch fetch", err, href);
+                  } catch (err: any) {
+                    if (err.code != "ENOTFOUND") {
+                      console.log("catch fetch", err, href);
+                    }
                     cb(err);
                   }
                 } else {
